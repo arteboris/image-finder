a1416db87ae9949b57ccd49baadd408a25f73174
arteboris <arteboris@ukr.net>
2019-10-16 16:53:35 +0300

    Updates

---
+++ precache-manifest.b6c1e5da2fc0b494519ac9b8cc8566f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d359d5674857648c80f583bfbbd60b10",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "6e1bdc84e0f21a6d388c",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.f27de11e.chunk.css"
+  },
+  {
+    "revision": "a62d36d77fd188fa428b",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.7a410f96.chunk.js"
+  },
+  {
+    "revision": "6e1bdc84e0f21a6d388c",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.dcf1ec87.chunk.js"
+  },
+  {
+    "revision": "7e3234e7aefa8359527f",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.3cf19af0.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/goit-react-hw-03-image-finder/precache-manifest.b6c1e5da2fc0b494519ac9b8cc8566f1.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-03-image-finder/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
